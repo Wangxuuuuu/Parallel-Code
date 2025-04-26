1f49a78c74bd0af5cba2714c6136b87ca7e96068
Xu Wang <2312166@mail.nankai.edu.cn>
2025-04-26 15:56:48 +0800

    Update main.cpp

--- 实验二-MD5Hash算法SIMD优化/串行版本/main.cpp
+++ 实验二-MD5Hash算法SIMD优化/串行版本/main.cpp
@@ -19,8 +19,8 @@ int main()
     double time_train = 0; // 模型训练的总时长
     PriorityQueue q;
     auto start_train = system_clock::now();
-    q.m.train("./input/Rockyou-singleLined-full.txt");
-    //q.m.train("/guessdata/Rockyou-singleLined-full.txt");
+    // q.m.train("./input/Rockyou-singleLined-full.txt");
+    q.m.train("/guessdata/Rockyou-singleLined-full.txt");
     q.m.order();
     auto end_train = system_clock::now();
     auto duration_train = duration_cast<microseconds>(end_train - start_train);
